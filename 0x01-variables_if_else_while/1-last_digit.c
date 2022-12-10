9a1e38b310d9eed9f819dcd0bf233113afe9fadd
Aziz TINDE <aziztinde@outlook.com>
2022-12-10 14:07:08 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ else
 	if (t == 0)
 	printf("Last digit of %d is %d and is 0\n", n, t);
 	else
-	printf("Last digit of %d is %d and less than 6 and not 0\n", n, t);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
 }
 	return (0);
 }
