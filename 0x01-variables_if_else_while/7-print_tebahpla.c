3dd889441b1ef546ab27ac7e1a5fd049f3e6bccd
Aziz TINDE <aziztinde@outlook.com>
2022-12-12 03:45:01 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - 7-print_tebahpla.c
+* Return: Allways 0
+*/
+int main(void)
+{
+	int i;
+
+for (i = 'z' ; i >= 'a' ; i--)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
