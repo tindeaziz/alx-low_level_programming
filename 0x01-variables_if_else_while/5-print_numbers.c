eeeb20a1d368aebc4039fec594a7a2f6b999c7c7
Aziz TINDE <aziztinde@outlook.com>
2022-12-12 03:29:12 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - 5-print_numbers.c
+* Return: Allways 0
+*/
+int main (void)
+{
+	int i;
+
+for (i = 0; i < 10; i++)
+{
+	printf("%d",i);
+}
+	putchar('\n');
+	return (0);
+}
