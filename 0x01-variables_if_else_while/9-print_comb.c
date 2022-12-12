ac0a9805110f1cc627f671fac5e0f54cabb8b8d8
Aziz TINDE <aziztinde@outlook.com>
2022-12-12 04:24:58 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,13 @@ int main(void)
 {
 	int i;
 
-for (i = '0' ; i <= '8' ; i++)
+for (i = '0' ; i <= '9' ; i++)
 {
 	putchar(i);
-	putchar(',');
+	if (i != '9')
+		putchar(',');
+	putchar(' ');
 }
-	putchar('9');
 	putchar('\n');
 	return (0);
 }
