62f633b0ab552b382bb859e25c568af60408616b
Aziz TINDE <aziztinde@outlook.com>
2022-12-12 02:53:31 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - 2-print_alphabet.c
+* Return: Allways 0
+*/
+int main(void)
+{
+	int n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+	putchar(n);
+}
+	putchar('\n');
+	return (0);
+}
