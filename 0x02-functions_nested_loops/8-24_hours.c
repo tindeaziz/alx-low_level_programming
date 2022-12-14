2dd90e2f7b95c60e04ab4803227676910348a40a
Aziz TINDE <aziztinde@outlook.com>
2022-12-14 08:33:31 -0800

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,9 @@ void jack_bauer(void)
 	int c;
 	int d;
 
-for (a = '0' ; a <= '2' ; a++)
+for (a = '0' ; a <= '1' ; a++)
 {
-	for (b = '0' ; b <= '3' ; b++)
+	for (b = '0' ; b <= '9' ; b++)
 {
 		for (c = '0' ; c <= '5' ; c++)
 {
@@ -26,6 +26,22 @@ for (a = '0' ; a <= '2' ; a++)
 				_putchar(d);
 				_putchar('\n');
 }
+}
+}
+}
+	a = '2';
+for (b ='0' ; b <= '3' ; b++)
+{
+	for (c = '0' ; c <= '5' ; c++)
+{
+		for (d = '0' ; d <= '9' ; d++)
+{
+			_putchar(a);
+			_putchar(b);
+			_putchar(':');
+			_putchar(c);
+			_putchar(d);
+			_putchar('\n');
 }
 }
 }
