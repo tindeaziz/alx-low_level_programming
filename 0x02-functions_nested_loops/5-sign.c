9f0ed5c088c832968778699c41619ab9c6750006
Aziz TINDE <aziztinde@outlook.com>
2022-12-14 04:14:52 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_sign - sign
+* @n: input
+* Return: 0 or 1
+*/
+
+int print_sign(int n)
+{
+if (n > '0')
+{
+	_putchar('+');
+	return (1);
+}
+else
+{
+	if (n < 0)
+{
+		_putchar('-');
+		return (-1);
+}
+	else
+{
+	_putchar('0');
+	return (0);
+}
+}
+}
