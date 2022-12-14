e1ea2a0b9bd9950ef820989a185ab09911526257
Aziz TINDE <aziztinde@outlook.com>
2022-12-14 05:48:00 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _abs - Absolute value
+* @int: Declaration
+ Return: Allways 0
+*/
+int _abs(int c)
+{
+if (c >= 0)
+{	return (c);
+}
+else
+{
+	return(-1*c);
+}
+	return (0);
+}
