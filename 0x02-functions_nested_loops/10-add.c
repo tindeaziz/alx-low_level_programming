2949a2e70d1b4c6b739c3069b1db9d5e8cf27187
Aziz TINDE <aziztinde@outlook.com>
2022-12-14 14:54:14 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - myfonction
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ * Return: Allways 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
