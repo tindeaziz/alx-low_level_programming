be837173a804c56340906686e7f767c1a06da2a1
Aziz TINDE <aziztinde@outlook.com>
2022-12-14 02:38:16 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _islower - Lower check caracter
+* @c: variable
+* Return: 0 or 1
+*/
+
+int _islower(int c)
+{
+	int i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+{
+	if (i == c)
+		return (1);
+	else if (i != c)
+		return (0);
+}
+	return (c);
+}
