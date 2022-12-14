87bf58fbfbb1c25262f58ddfaf20cb3c7cc8aab8
Aziz TINDE <aziztinde@outlook.com>
2022-12-14 08:02:46 -0800

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* jack_bauer - Chrono code
+* Return: Allways 0;
+*/
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+for (a = '0' ; a <= '2' ; a++)
+{
+	for (b = '0' ; b <= '3' ; b++)
+{
+		for (c = '0' ; c <= '5' ; c++)
+{
+			for (d = '0' ; d <='9' ; d++)
+{
+				_putchar(a);
+				_putchar(b);
+				_putchar(':');
+				_putchar(c);
+				_putchar(d);
+				_putchar('\n');
+}
+}
+}
+}
+}
