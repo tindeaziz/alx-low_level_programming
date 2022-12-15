c7ec9a6de68aea78b2973913eb1182ae20ae8b4a
Aziz TINDE <aziztinde@outlook.com>
2022-12-15 07:40:01 -0800

    Debug 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,13 +14,12 @@ int largest_number(int a, int b, int c)
 {
 	largest = a;
 }
-	else if (b > a && b > c)
-{
-	largest = b;
-}
-	else if (c > a && c > b)
-{
-	largest = c;
+	else
+{ 
+		if (b > a && b > c)
+			largest = b;
+		else
+			largest = c;
 }
 	return (largest);
 }
