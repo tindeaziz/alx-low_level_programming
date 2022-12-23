18d14698492ae0e7feb6e3e9337b7336ddbe6960
Aziz TINDE <aziztinde@outlook.com>
2022-12-23 14:08:29 -0800

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,6 @@
 int _strlen(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
